e9d3cd08344b8eb59938ef5aa6cf17f0e4a37c4a
Thiago <fthiagorodrigues10@gmail.com>
2022-10-04 15:26:02 -0300

    entrega

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,93 @@
-/* Desenvolva sua lógica aqui ... */
+import { products } from "../../scripts/productsData.js";
+import { categories } from "../../scripts/productsData.js";
+
+function showProducts(products) {
+	const productsList = document.getElementById("js-products-list");
+
+	return (productsList.innerHTML = products
+		.map((product) => {
+			return `<li class="card" id="${product.category}">
+    <div class="card-image">
+      <img src="${product.img}" alt="img">
+    </div>
+    <div class="card-header">
+      <h3 class="font-3">${product.band}</h2>
+        <span class="font-4"${product.year}</span>
+    </div>
+    <div class="card-body">
+      <h2 class="font-2">${product.title}</h2>
+    </div>
+    <div class="card-footer">
+      <span class="font-3">R$ ${product.price}.00</span>
+      <button class="card-btn">Comprar</button>
+    </div>
+  </li>`;
+		})
+		.join(" "));
+}
+showProducts(products);
+
+function showCategories(categories) {
+	const categoriesList = document.getElementById("js-categories-list");
+
+	categories.forEach((category) => {
+		categoriesList.insertAdjacentHTML(
+			"beforeend",
+			`<li class="category-item"><button class="categories-btn">${category}</button></li>`
+		);
+	});
+}
+showCategories(categories);
+
+function filter() {
+	const categoriesList = document.querySelectorAll(".category-item button");
+
+	categoriesList[0].classList.add("active");
+
+	categoriesList.forEach((btn) =>
+		btn.addEventListener("click", handleFilterCategory)
+	);
+
+	function handleFilterCategory(e) {
+		categoriesList.forEach((btn) => btn.classList.remove("active"));
+
+		this.classList.add("active");
+		const category = categories.indexOf(e.target.innerHTML);
+
+		const filteredProducts = products.filter(
+			(item) => item.category === category
+		);
+		showProducts(filteredProducts);
+		total(filteredProducts);
+
+		if (e.target.innerHTML === "Todos") {
+			showProducts(products);
+			total();
+		}
+	}
+}
+
+filter();
+
+function total(array = products) {
+	const filterValue = document.querySelector(".filter-price");
+	const filterInput = document.querySelector(".filter-range");
+	const productPrices = document.querySelectorAll(".card-footer span");
+
+	let totalPrice = Array.from(productPrices).reduce(
+		(a, b) => a + +b.innerHTML.slice(2),
+		0
+	);
+
+	filterInput.setAttribute("max", totalPrice);
+
+	filterInput.addEventListener("input", handleFilter);
+
+	function handleFilter() {
+		let value = filterInput.value;
+		filterValue.innerHTML = `Até R$ ${value}`;
+
+		showProducts(array.filter((item) => item.price >= value));
+	}
+}
+total();
--- scripts/theme.js
+++ scripts/theme.js
@@ -1,1 +1,41 @@
 /* Desenvolva sua lógica aqui ... */
+
+const button = document.getElementById("theme");
+const body = document.body;
+const theme = "dark-mode";
+const themeButtonIcon = [
+	"../../assets/img/sun.svg",
+	"../../assets/img/moon.svg",
+];
+
+let darkMode;
+
+button.addEventListener("click", themeChange);
+
+function themeChange() {
+	darkMode = !darkMode;
+
+	body.classList.toggle(theme);
+
+	localStorage.setItem(theme, darkMode);
+
+	themeChangeButtonIcon(button);
+}
+
+function themeChangeButtonIcon(button) {
+	darkMode
+		? (button.querySelector("img").src = themeButtonIcon[0])
+		: (button.querySelector("img").src = themeButtonIcon[1]);
+}
+
+function themePreferenceAnalysis() {
+	darkMode = JSON.parse(localStorage.getItem(theme));
+
+	if (darkMode) {
+		themeChangeButtonIcon(button);
+		body.classList.add(theme);
+	} else {
+		themeChangeButtonIcon(button);
+	}
+}
+themePreferenceAnalysis();
